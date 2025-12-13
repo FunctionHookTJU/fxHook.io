028917688b4d514e359909108ce5b3c3cefbfbb8
函钩 <tjdx1225230685@tju.edu.cn>
2025-12-13 22:13:48 +0800

    d

--- scripts/backgroundLoader.js
+++ scripts/backgroundLoader.js
@@ -43,7 +43,7 @@ class BackgroundLoader {
             left: 0;
             width: 100%;
             height: 100%;
-            z-index: -1;
+            z-index: -2;
             overflow: hidden;
         `;
         
